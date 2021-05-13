e4c301c0b14eda96ff40ac78b6fb899ff2636f55
kvn <>
2021-05-13 14:54:32 -0700

    Add a script to burn dvds
    Because I always have to look up the command for creating an iso and this makes it slightly easier

---
+++ mkdvd.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Create an iso and burn a disc for playback on a conventional dvd player
+# "$1" -- A folder containing dvd video files
+MOVIE=$(basename "$1")
+DISC_NAME=$(echo -n "$MOVIE" | tr [:lower:] [:upper:] | tr "\-." " " | tr -d [:punct:] | tr " " "_")
+OUTFILE="${MOVIE}.dvd5.iso"
+mkisofs -dvd-video -udf -V "$DISC_NAME" -o "./$OUTFILE" "$1" && \
+while true; do
+    read -p "Burn a DVD? [y/N] " yn
+    case $yn in
+        [Yy] ) wodim speed=4 dev=/dev/cdrom -v -data "./$OUTFILE"; break;;
+        [Nn] | '' ) printf "Burn a disc later with:\n\t wodim speed=4 $OUTFILE\n"; exit;; 
+    esac
+done
