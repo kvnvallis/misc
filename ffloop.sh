f8e670fca85d80415bd10763be5ddb1223316d00
kvn <>
2021-11-29 13:29:42 -0800

    output to current dir instead of src dir
    and allow opts for input file

--- ffloop.sh
+++ ffloop.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env sh
 #
-# A generic processing loop for running ffmpeg on multiple files
+# A generic processing loop for running ffmpeg on multiple files. Outputs to
+# current directory.
 #
 # Useage:
 #     ffloop.sh ./*.mkv
 
 extension=".mp4"  # file type for output files
-opts="-map 0:v -map 0:a -c copy -map_chapters -1"  # options supplied to ffmpeg
+opts_out="-map 0:v -map 0:a -c copy -map_chapters -1"
+opts_in=""
 
 for infile in "$@"; do
+    extfilename=$(basename -- "$infile")
     filename="${infile%.*}"
     outfile="${filename}${extension}"
-    ffmpeg -i "${infile}" ${opts} "${outfile}"
+    ffmpeg ${opts_in} -i "${infile}" ${opts_out} "${outfile}"
 done
