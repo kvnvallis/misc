e90d9c46f07aa53b4fcee29636b4973aa0d844e4
kvn <>
2021-11-29 18:55:05 -0800

    Allow reading input files from a list
    You can now supply a list of video files on the command line, or a single text file containing a newline-delimited list of video files

--- ffloop.sh
+++ ffloop.sh
@@ -7,12 +7,26 @@
 #     ffloop.sh ./*.mkv
 
 extension=".mp4"  # file type for output files
-opts_out="-map 0:v -map 0:a -c copy -map_chapters -1"
+opts_out="-map 0:v -map 0:a -c copy -map_chapters -1" # options applied to ffmpeg output
 opts_in=""
 
-for infile in "$@"; do
-    extfilename=$(basename -- "$infile")
-    filename="${infile%.*}"
-    outfile="${filename}${extension}"
-    ffmpeg ${opts_in} -i "${infile}" ${opts_out} "${outfile}"
-done
+run_ffmpeg() {
+    video_in="$1"
+    filename_dot_ext=$(basename -- "$video_in")
+    filename="${filename_dot_ext%.*}"
+    video_out="${filename}${extension}"
+    ffmpeg -nostdin $opts_in -i "${video_in}" ${opts_out} "${video_out}"
+}
+
+## Read each line of a file as input to ffmpeg
+if [ $# == 1 ] && [ -f "$1" ] && [ ${1##*.} == 'txt' ]; then
+    args_file="$1"
+    while read infile; do
+        run_ffmpeg "$infile"
+    done < "$args_file"
+## Read input files from the command line
+else
+    for infile in "$@"; do
+        run_ffmpeg "$infile"
+    done
+fi
