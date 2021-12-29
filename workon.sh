bb9722ee5065353e52005e77b26ccd5d43157bda
kvn <>
2021-12-29 14:35:43 -0800

    Put workon.sh code into a function
    Sourcing the activate script for your current shell only works if you
    also source workon.sh

--- workon.sh
+++ workon.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env sh
 #
 # A cheap version of virtualenvwrapper's workon command
+# Source this file or copy it into your .bashrc
 
-if [ $# -eq 0 ]; then
-    echo No arguments given to $(basename "$0"); exit 1
-fi
+workon() {
+    if [ $# -eq 0 ]; then
+        echo No arguments given to $(basename "$0"); return
+    fi
 
-project="$1"
-workon_home="$HOME/Pyvenvs/"
+    project="$1"
+    workon_home="$HOME/Pyvenvs/"
 
-if [ -d "${workon_home}/${project}" ]; then
-    source "${workon_home}/${project}/Scripts/activate"
-else
-    echo No project found in $workon_home by name $project
-fi
-
+    if [ -d "${workon_home}/${project}" ]; then
+        source "${workon_home}/${project}/Scripts/activate"
+    else
+        echo No project found in $workon_home by name $project
+    fi
+}
