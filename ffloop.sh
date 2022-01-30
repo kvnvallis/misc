d6010482d9bf9caf02b1cb8fed7d9b24451b422c
kvn <>
2022-01-30 01:35:49 -0800

    Use eval set to properly expand filenames
    Get rid of boolean logic for dry run because it behaves differently than
    an if/else.
    
    Check if video file already exists and skip it, so we can leave an
    encoded file in place and extract subtitles or audio later.
    
    Use -eq for numerical comparison instead of = which is for string
    comparison

--- ffloop.sh
+++ ffloop.sh
@@ -6,21 +6,18 @@
 #	  $ ffloop.sh ./*.mkv
 #	  $ ffloop.sh ./files.txt
 
-# __NOTE:__ ffmpeg 4.4 won't read filenames with single quotes in them ('). This was tested on windows in a mingw64 terminal using `ffprobe` alone. The bug has nothing to do with this script.
-
-
 ### USER CONFIG ###
 
-DRY_RUN=true  # echo commands but don't run them
+DRY_RUN=false  # echo commands but don't run them
 TWO_PASS=false
 EXTRACT_SUBS=true
 EXTRACT_AUDIO=false
 VID_EXTENSION=".mkv"
 SUB_EXTENSION=".srt"
 AUD_EXTENSION=""
-VIDOUT_OPTS="-map 0:v:0 -map 0:a:0 -c:a copy -c:v libx264 -s 640x480 -aspect 4:3 -crf 18"
-SUBOUT_OPTS="-map 0:s:0 -c:s text"
-AUDOUT_OPTS=""
+VIDOUT_OPTS="-map 0:v:0 -map 0:a:1 -c:a copy -c:v libx264 -s 640x480 -crf 18"
+SUBOUT_OPTS="-map 0:s:1 -c:s text"
+AUDOUT_OPTS="-c:a libmp3lame -q:a 1"
 OPTS_IN=""	# applied to input; usually not needed
 
 ### END OF USER CONFIG ###
@@ -28,46 +25,65 @@ OPTS_IN=""	# applied to input; usually not needed
 
 run_ffmpeg() {
 	# Takes one argument, the name of the input file to ffmpeg
+	if [ ! -f "$1" ] || [ $# -gt 1 ]; then
+		echo Is not a file: "$1"
+		return
+	fi
+
 	video_in="$1"
 	filename_dot_ext=$(basename -- "$video_in")
 	filename="${filename_dot_ext%.*}"
 	video_out="${filename}${VID_EXTENSION}"
 	subtitle_out="${filename}${SUB_EXTENSION}"
 	audio_out="${filename}${AUD_EXTENSION}"
+	base_cmd="-nostdin $OPTS_IN -i \"$video_in\""
 
-	# Assign ffmpeg arguments to function's positional parameters, so we can build the command line piece by piece.
-	set -- -nostdin $OPTS_IN -i "$video_in"
+	# Assign ffmpeg arguments to function's positional parameters, so we can build the command line piece by piece. Use of eval is required for $video_in filesname to expand properly.
+	eval set -- "$base_cmd"
 
-	if [ "$TWO_PASS" = true ]; then
-		pass_count=1
-		set -- "$@" $VIDOUT_OPTS -pass $pass_count -an -f null -y /dev/null
-		[ "$DRY_RUN" != true ] && ffmpeg "$@" || echo ffmpeg "$@"
-		pass_count=$((pass_count+1))
-	fi
+		if [ "$TWO_PASS" = 'true' ]; then
+			pass_count=1
+			set -- "$@" $VIDOUT_OPTS -pass $pass_count -an -f null -y /dev/null
+			if [ "$DRY_RUN" != 'true' ]; then
+				ffmpeg "$@"
+			else
+				echo ffmpeg "$@"
+			fi
+			pass_count=$((pass_count+1))
+		fi
 
-	if [ "$pass_count" = 2 ]; then
-		# reset to base ffmpeg command
-		set -- -nostdin $OPTS_IN -i "$video_in"
-		set -- "$@" $VIDOUT_OPTS -pass $pass_count "$video_out"
+	# Test if video file exists, because sometimes we want to leave it in place while extracting audio or subtitles, without also re-encoding the video.
+	if [ ! -f "$video_out" ]; then
+		if [ "$pass_count" -eq 2 ]; then
+			eval set -- "$base_cmd"
+			set -- "$@" $VIDOUT_OPTS -pass $pass_count "$video_out"
+		else
+			# same thing as above but without -pass
+			set -- "$@" $VIDOUT_OPTS "$video_out"
+		fi
 	else
-		# same thing as above but without -pass
-		set -- "$@" $VIDOUT_OPTS "$video_out"
+		echo "Video file already exists."
+
 	fi
 
-	if [ "$EXTRACT_SUBS" = true ]; then
+	if [ "$EXTRACT_SUBS" = 'true' ]; then
 		set -- "$@" $SUBOUT_OPTS "$subtitle_out"
 	fi
 
-	if [ "$EXTRACT_AUDIO" = true ]; then
+	if [ "$EXTRACT_AUDIO" = 'true' ]; then
 		set -- "$@" $AUDOUT_OPTS "$audio_out"
 	fi
 
-	[ "$DRY_RUN" != true ] && ffmpeg "$@" || echo ffmpeg "$@"
+	if [ "$DRY_RUN" != 'true' ]; then
+		ffmpeg "$@"
+	else
+		echo ffmpeg "$@"
+	fi
 }
 
 
 # Read each line of a file as input to ffmpeg
-if [ $# == 1 ] && [ -f "$1" ] && [ ${1##*.} == 'txt' ]; then
+if [ $# -eq 1 ] && [ -f "$1" ] && [ "${1##*.}" = 'txt' ]; then
 	args_file="$1"
 	while read infile; do
 		run_ffmpeg "$infile"
