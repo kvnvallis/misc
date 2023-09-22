98385489d1fe3f027511c006f7d89e5e1790b989
kvn <>
2023-09-21 23:09:04 -0700

    Discord install script for those too-frequent updates

---
+++ discord-inst.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Download and install latest discord tar.gz file to $destdir/Discord
+
+destdir="$HOME/Apps/"
+
+if [ -d "$destdir" ]; then
+    rm -r "$destdir/Discord" 2>/dev/null &&
+    echo "Old version removed: $_"
+    wget --content-disposition -q -O - "https://discord.com/api/download?platform=linux&format=tar.gz" | 
+    tar xvz -C "$destdir" &&
+    echo New version finished installing ||
+    echo ERROR: Installation failed
+fi
