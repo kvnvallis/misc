e77496de52e26e4a421383898f6398580fc2b821
kvn <>
2023-11-20 21:03:16 -0800

    check for empty destdir and exit 1 on error

--- discord-inst.sh
+++ discord-inst.sh
@@ -4,13 +4,20 @@
 
 destdir="$HOME/Apps/"
 
-if [ -d "$destdir" ]; then
+if [ -z "$destdir" ]; then
+    echo ERROR: No destination defined
+    exit 1
+elif [ -d "$destdir" ]; then
     rm -r "$destdir/Discord" 2>/dev/null &&
     echo "Old version removed: $_"
     wget --content-disposition -q -O - "https://discord.com/api/download?platform=linux&format=tar.gz" | 
     tar xvz -C "$destdir" &&
     echo New version finished installing ||
-    echo ERROR: Installation failed
+    echo ERROR: Installation failed &&
+    exit 1
 else
     echo "Invalid destination: $destdir"
+    exit 1
 fi
+
+exit 0
